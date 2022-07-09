a3d92fc45b12d51efc8b508af1c15727a63354b7
B1g0neZzz <gzhh0216@gmail.com>
2022-07-09 11:13:13 +0800

    创建仓库

---
+++ src/server/chatservice.cpp
@@ -0,0 +1,48 @@
+#include "chatservice.hpp"
+#include "public.hpp"
+#include <string>
+#include <muduo/base/Logging.h>
+using namespace muduo;
+
+ChatService *ChatService::instance()
+{
+    static ChatService service;
+    return &service;
+}
+
+ChatService::ChatService()
+{
+    _msgHandlerMap.insert({LOGIN_MSG, std::bind(&ChatService::login, this, _1, _2, _3)});
+    _msgHandlerMap.insert({REG_MSG, std::bind(&ChatService::reg, this, _1, _2, _3)});
+}
+
+// 获取消息对应的处理器
+MsgHandler ChatService::getHandler(int msgid)
+{
+    // 记录错误日志，msgid 没有对应的事件处理回调
+    auto it = _msgHandlerMap.find(msgid);
+    if (it == _msgHandlerMap.end())
+    {
+        // 返回一个默认的处理器，空操作
+        return [=](const TcpConnectionPtr &conn, json &js, Timestamp)
+        {
+            LOG_ERROR << "msgid:" << msgid << " can not find handler";
+        };
+    }
+    else
+    {
+        return _msgHandlerMap[msgid];
+    }
+}
+
+// 处理登录业务
+void ChatService::login(const TcpConnectionPtr &conn, json &js, Timestamp time)
+{
+    LOG_INFO << "do login service!!!";
+}
+
+// 处理注册业务
+void ChatService::reg(const TcpConnectionPtr &conn, json &js, Timestamp time)
+{
+    LOG_INFO << "do reg service!!!";
+}
